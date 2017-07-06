e74352d3957dce3a6044dea74457a5c2c2bd439a
Tyson Gern <tgern@pivotal.io>
2017-07-06 10:44:44 -0600

    Initial commit

---
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/RegistrationApp.java
@@ -0,0 +1,51 @@
+package io.pivotal.pal.tracker.registration;
+
+import io.pivotal.pal.tracker.projects.data.ProjectDataGateway;
+import io.pivotal.pal.tracker.projects.data.ProjectFields;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+import static io.pivotal.pal.tracker.projects.data.ProjectFields.projectFieldsBuilder;
+
+
+@SpringBootApplication
+@ComponentScan({
+    "io.pivotal.pal.tracker.accounts",
+    "io.pivotal.pal.tracker.restsupport",
+    "io.pivotal.pal.tracker.projects",
+    "io.pivotal.pal.tracker.users"
+})
+public class RegistrationApp {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(RegistrationApp.class, args);
+    }
+
+    private final ProjectDataGateway gateway;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public RegistrationApp(ProjectDataGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    @PostConstruct
+    public void init(){
+        // Make sure there is data in the registration server when
+        // it starts.
+        ProjectFields project = projectFieldsBuilder()
+                                    .accountId(1)
+                                    .name("Basket Weaving")
+                                    .build();
+        logger.info("**********************************");
+        logger.info("Creating project: " + project);
+        logger.info("**********************************");
+        gateway.create(project);
+    }
+
+}
---
+++ components/users/src/test/java/test/pivotal/pal/tracker/users/data/UserDataGatewayTest.java
@@ -0,0 +1,31 @@
+package test.pivotal.pal.tracker.users.data;
+
+
+import io.pivotal.pal.tracker.users.data.UserDataGateway;
+import io.pivotal.pal.tracker.users.data.UserRecord;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDataGatewayTest {
+
+    private UserDataGateway gateway = new UserDataGateway();
+
+    @Test
+    public void testCreate() {
+        UserRecord createdUser = gateway.create("aUser");
+
+        assertThat(createdUser.id).isGreaterThan(0);
+        assertThat(createdUser.name).isEqualTo("aUser");
+
+        UserRecord found = gateway.find(createdUser.id);
+
+        assertThat(found.id).isEqualTo(createdUser.id);
+        assertThat(found.name).isEqualTo(createdUser.name);
+    }
+
+    @Test
+    public void testFind_WhenNotFound() {
+        assertThat(gateway.find(42347L)).isNull();
+    }
+}

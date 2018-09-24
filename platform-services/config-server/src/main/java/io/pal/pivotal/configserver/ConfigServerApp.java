78c77d0f87015cabcaca554d0fe51fee9ec50c65
gasper_lf@hotmail.com <lewis.florez@globant.com>
2018-09-24 13:00:58 -0500

    2. Commit your change. falta resto de puntos and test

---
+++ platform-services/config-server/src/main/java/io/pal/pivotal/configserver/ConfigServerApp.java
@@ -0,0 +1,14 @@
+package io.pal.pivotal.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class, args);
+    }
+}
